d5ff94095f499ef250ee0049e905982757e01adb
Pats-lang <patriciapascual031@gmail.com>
2024-04-18 02:20:27 +0800

    Update u907822938_barangaydb.sql

--- database/u907822938_barangaydb.sql
+++ database/u907822938_barangaydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2024 at 04:04 PM
+-- Generation Time: Apr 17, 2024 at 08:20 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -24,23 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin_users`
+-- Table structure for table `admin`
 --
 
-CREATE TABLE `admin_users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(150) NOT NULL,
-  `password` varchar(150) NOT NULL,
-  `fullname` varchar(100) NOT NULL,
-  `role` int(2) NOT NULL
+CREATE TABLE `admin` (
+  `admin_username` varchar(20) NOT NULL,
+  `admin_password` varchar(100) NOT NULL,
+  `admin_fullname` varchar(20) NOT NULL,
+  `admin` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `admin_users`
+-- Dumping data for table `admin`
 --
 
-INSERT INTO `admin_users` (`id`, `username`, `password`, `fullname`, `role`) VALUES
-(1, 'admin1', '$2y$10$zs9N951Be4tU4vEeLK9zvuBbh3yOeMigbjWSueHs/IlKzMiocbA.W', '', 0);
+INSERT INTO `admin` (`admin_username`, `admin_password`, `admin_fullname`, `admin`) VALUES
+('araa', '$2y$10$vxZV5xLcIzxlqCf4x.qdAeQHy5jjuUqRWKDquhN51p4e/vbbLjqCG', 'Arabella Belardo', '1'),
+('layka', '$2y$10$aYj1V1PWlNaQMxHXvRES/.2xnTPLvlXfO9pYzMLTOFgEsnTGEEM22', 'Laica Mae Martinez', '1'),
+('rona', '$2y$10$GnJysKPQ2wUkY/yEws7UM.6da34n4Z6Y68wsw4tAdlPVWSLo5BLVi', 'Ronalaine Villaluna', '1'),
+('rona26', '$2y$10$iRwInEkIxvg6v77nv9hoTu44LEiVKry0rUHRVQlVXdYzt35OBc1Em', 'Ronalaine N. Villalu', '2');
 
 -- --------------------------------------------------------
 
@@ -64,6 +66,41 @@ INSERT INTO `announcement` (`id`, `img`, `title`, `description`, `last_modified`
 (1, '421942795_939844190823403_4852506872741483609_n.jpg', 'Barangay 20', 'Welcome!', '2024-04-05 06:06:35'),
 (5, 'officials.jpg', 'Barangay Officials', '', '2024-03-26 07:53:10'),
 (7, 'received_586949182760855-1.jpeg', 'SK', '', '2024-03-31 19:09:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `change_logs`
+--
+
+CREATE TABLE `change_logs` (
+  `id` int(10) NOT NULL,
+  `admin` varchar(20) NOT NULL,
+  `operation` varchar(20) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `change_logs`
+--
+
+INSERT INTO `change_logs` (`id`, `admin`, `operation`, `description`, `timestamp`) VALUES
+(1, 'rona', 'add', 'Admin: <b>LAYKA</b> has been added to <b>System Administrators.</b>', '2024-04-16 02:36:13'),
+(2, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-16 02:46:04'),
+(3, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-16 02:46:49'),
+(4, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-16 02:49:54'),
+(5, 'rona', 'add', 'Alumni Member: <b>2024801</b> have been registered at  <b>Alumni Members.</b>', '2024-04-16 04:15:59'),
+(6, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-16 05:05:34'),
+(7, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-16 05:10:22'),
+(8, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-16 14:57:05'),
+(9, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-16 17:18:01'),
+(10, 'rona', 'login', 'Admin: <b>RONA</b> Just logged on to the System', '2024-04-17 14:45:55'),
+(11, 'rona', 'add', 'Admin: <b>ARAA</b> has been added to <b>System Administrators.</b>', '2024-04-17 15:16:47'),
+(12, 'araa', 'login', 'Admin: <b>ARAA</b> Just logged on to the System', '2024-04-17 15:17:01'),
+(13, 'araa', 'add', 'Alumni Member: <b>20241302</b> have been registered at  <b>Alumni Members.</b>', '2024-04-17 15:52:36'),
+(14, 'araa', 'add', 'Alumni Member: <b>2024401</b> have been registered at  <b>Alumni Members.</b>', '2024-04-17 15:53:51'),
+(15, 'araa', 'add', 'Alumni Member: <b>2024402</b> have been registered at  <b>Alumni Members.</b>', '2024-04-17 18:00:19');
 
 -- --------------------------------------------------------
 
@@ -185,7 +222,156 @@ INSERT INTO `inquire` (`id`, `i_name`, `i_email`, `i_message`, `r_message`, `i_s
 (31, 'Paolo Rafael Salazar Tampico', 'paolorafaeltampico@gmail.com', 'Hello', 'Hi', 1),
 (32, 'barangay20', 'barangay020@gmail.com', 'sample format', 'testing', 1),
 (33, 'Layka', 'Martinezlaicamae17@gmail.com', 'bhjbjhuhgu', 'hello', 1),
-(34, 'ronaaaaaaaaa', 'rsales059@gmail.com', 'hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii', 'helooooooooooooooo', 1);
+(34, 'ronaaaaaaaaa', 'rsales059@gmail.com', 'hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii', 'helooooooooooooooo', 1),
+(35, 'fdfsf', 'rsales059@gmail.com', 'dsadadsdadad', 'dsfsfsfsfsfsfsfsfsf', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `member_id` int(20) NOT NULL,
+  `year` int(10) NOT NULL,
+  `member_count` int(5) NOT NULL,
+  `campus_id` varchar(5) DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
+  `precinct` varchar(10) NOT NULL,
+  `birth_date` date NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `civil_status` varchar(20) NOT NULL,
+  `religion` varchar(100) NOT NULL,
+  `email_address` varchar(50) NOT NULL,
+  `cellphone_no` varchar(20) NOT NULL,
+  `picture` varchar(100) NOT NULL,
+  `signature` varchar(100) NOT NULL,
+  `time_registered` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` int(2) NOT NULL,
+  `cid` int(2) NOT NULL,
+  `idfront` varchar(100) NOT NULL,
+  `idback` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`member_id`, `year`, `member_count`, `campus_id`, `name`, `precinct`, `birth_date`, `address`, `civil_status`, `religion`, `email_address`, `cellphone_no`, `picture`, `signature`, `time_registered`, `status`, `cid`, `idfront`, `idback`) VALUES
+(2024101, 2024, 1, '01', 'Wylie Scott', 'Aliqua', '1995-01-08', 'Voluptas aperiam vol', 'LIVING-IN', 'Jehovah\\\'s Witnesses', 'konysi@mailinator.com', '09567575675', 'avatar1.png', 'signature1.png', '2024-04-17 16:51:58', 0, 1, '', ''),
+(2024102, 2024, 1, '02', 'Basil Duran', 'Expedi', '1981-06-22', 'Rerum molestiae moll', 'Legally Separated', 'Baptist', 'vejofyv@mailinator.com', '09546546456', 'avatar1.png', 'avatar2.png', '2024-04-17 17:03:19', 0, 1, '', ''),
+(2024201, 2024, 2, '01', 'Renee Case', 'Exerc7', '1996-12-09', 'Nisi voluptas tenetu', 'Others', 'Jehovah\\\'s Witnesses', 'kylyxuc@mailinator.com', '09454353454', 'avatar5.png', 'signature2.png', '2024-04-17 17:11:59', 0, 1, '', ''),
+(2024202, 2024, 2, '02', 'Noble Hart', 'Assu56', '2006-07-30', 'Veritatis ut amet m', 'Single', 'Dating Daan', 'vipujyv@mailinator.com', '09454534444', 'avatar2.png', 'signature1.png', '2024-04-17 17:06:21', 0, 1, '', ''),
+(2024301, 2024, 3, '01', 'Blair Reid', 'Qui454', '2016-03-12', 'Exercitation possimu', 'Widow/er', 'Iglesia ni Cristo', 'jyjyha@mailinator.com', '09543453432', 'avatar5.png', 'signature1.png', '2024-04-17 17:19:11', 0, 1, '', ''),
+(2024302, 2024, 3, '02', 'Kylie Bonner', 'Est sy', '1988-05-30', 'Cumque omnis explica', 'Separated', 'Baptist', 'rsales059@gmail.com', '09545445444', 'avatar5.png', 'signature1.png', '2024-04-17 17:51:55', 0, 1, '', ''),
+(2024401, 2024, 4, '01', 'Hakeem Howard', 'Corrup', '1994-08-17', 'Sint molestiae enim', 'Others', 'Iglesia ni Cristo', 'pera@mailinator.com', '09765656756', 'avatar1.png', 'signature1.png', '2024-04-17 17:57:14', 0, 1, '', ''),
+(2024402, 2024, 4, '02', 'Zephania Calderon', '667676', '1997-12-24', 'Hic officia adipisci', 'LIVING-IN', 'Jehovah\\\'s Witnesses', 'dyzer@mailinator.com', '09767868686', 'avatar6.png', 'avatar7.png', '2024-04-17 18:00:19', 0, 1, '', ''),
+(2024501, 2024, 5, '01', 'Morgan Lee', 'Volu66', '1970-02-01', 'Ea velit tempor do', 'Legally Separated', 'Jehovah\\\'s Witnesses', 'rsales059@gmail.com', '09545645444', 'avatar8.png', 'signature5.png', '2024-04-17 18:11:47', 0, 1, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_account`
+--
+
+CREATE TABLE `member_account` (
+  `id` int(20) NOT NULL,
+  `member_id` int(20) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member_account`
+--
+
+INSERT INTO `member_account` (`id`, `member_id`, `username`, `password`) VALUES
+(1, 2024101, '', ''),
+(2, 2024102, '', ''),
+(3, 2024202, '', ''),
+(4, 2024201, '', ''),
+(5, 2024301, '', ''),
+(6, 2024401, 'rona', '$2y$10$82Xha1o4p4sWj/i9BiESBeqUirCXg3xpFZ5GJ7m9Kjz'),
+(7, 2024402, 'araa', '$2y$10$85s8pFPS3p86Kkn8lCsMueoLt1/bMEd485Kp0PKmDN6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_address`
+--
+
+CREATE TABLE `member_address` (
+  `id` int(20) NOT NULL,
+  `member_id` int(20) DEFAULT NULL,
+  `residency` varchar(50) NOT NULL,
+  `yrs_res` varchar(20) NOT NULL,
+  `postal` varchar(50) NOT NULL,
+  `district` varchar(50) NOT NULL,
+  `brgy` varchar(50) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `province` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member_address`
+--
+
+INSERT INTO `member_address` (`id`, `member_id`, `residency`, `yrs_res`, `postal`, `district`, `brgy`, `region`, `province`, `city`) VALUES
+(1, 2024101, 'Helper', '1990', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(2, 2024102, 'Constraction Worker', '1989', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(3, 2024202, 'Home Owner', '2012', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(4, 2024201, 'Home Owner', '1973', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(5, 2024301, 'Others', '2005', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(6, 2024302, 'Constraction Worker', '1983', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(7, 2024302, 'Constraction Worker', '1983', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(8, 2024401, 'Constraction Worker', '1977', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(9, 2024402, 'Tenant', '1975', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City'),
+(10, 2024501, 'Tenant', '1983', '1400', 'District II', 'Barangay 20', 'Metro Manila', 'Metro Manila', 'Caloocan City');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_emergency`
+--
+
+CREATE TABLE `member_emergency` (
+  `id` int(20) NOT NULL,
+  `member_id` int(20) DEFAULT NULL,
+  `contact_name` varchar(50) NOT NULL,
+  `contact_relation` varchar(50) NOT NULL,
+  `contact_no` varchar(20) NOT NULL,
+  `contact_address` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member_emergency`
+--
+
+INSERT INTO `member_emergency` (`id`, `member_id`, `contact_name`, `contact_relation`, `contact_no`, `contact_address`) VALUES
+(1, 2024101, 'Orli Ortiz', 'Perspiciatis esse', '09565766666', 'Facilis qui nostrud'),
+(2, 2024102, 'Quynn Padilla', 'Voluptatem id conseq', '09454354354', 'Nostrum deserunt nat'),
+(3, 2024202, 'Adena Gray', 'Aut obcaecati conseq', '09454353454', 'Odit magna ullam rep'),
+(4, 2024201, 'Illana Cole', 'Non mollit consequat', '09435434444', 'Qui rerum temporibus'),
+(5, 2024301, 'Doris Mcdowell', 'Proident ex possimu', '09546546545', 'Suscipit odit conseq'),
+(6, 2024302, 'Moana Hayes', 'Provident expedita', '09656565655', 'Porro illum consequ'),
+(7, 2024302, 'Moana Hayes', 'Provident expedita', '09656565655', 'Porro illum consequ'),
+(8, 2024401, 'Bryar Odonnell', 'Quos voluptatibus ve', '09565765655', 'Nesciunt lorem prov'),
+(9, 2024402, 'Knox Carrillo', 'In at ut autem sint', '09454654767', ''),
+(10, 2024501, 'Patricia George', 'Iusto in in laborum', '09656565555', 'Tempore reprehender');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_proof`
+--
+
+CREATE TABLE `member_proof` (
+  `id` int(20) NOT NULL,
+  `member_id` int(20) DEFAULT NULL,
+  `valid_id` varchar(50) NOT NULL,
+  `proof_residency` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -243,6 +429,39 @@ INSERT INTO `projects` (`id`, `img`, `title`, `description`, `last_modified`) VA
 (18, 'palengke 2.jpg', 'Barangay 20 Palengke', '', '2024-03-25 11:58:38'),
 (19, 'palengke.jpg', 'Barangay 20 Palengke', '', '2024-03-25 11:59:04'),
 (20, 'palengke 3.jpg', 'Barangay 20 Palengke', '', '2024-03-25 12:01:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proof`
+--
+
+CREATE TABLE `proof` (
+  `id` int(20) NOT NULL,
+  `member_id` int(20) DEFAULT NULL,
+  `valid_id` varchar(100) NOT NULL,
+  `proof_residency` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `proof`
+--
+
+INSERT INTO `proof` (`id`, `member_id`, `valid_id`, `proof_residency`) VALUES
+(39, 20232201, '', ''),
+(42, 20232301, '', ''),
+(54, 2023102, 'workComA', 'workPosA'),
+(55, 2023102, 'workComB', 'workPosB'),
+(56, 20232401, '', ''),
+(57, 20232501, '', ''),
+(58, 2023202, '', ''),
+(59, 2023302, '', ''),
+(60, 2023402, '', ''),
+(61, 2024502, '', ''),
+(62, 2024602, 'Macias and Aguirre Traders', 'Quasi quae non vel e'),
+(63, 2024602, 'Macias and Aguirre Traders', 'Quasi quae non vel e'),
+(64, 2024602, 'Macias and Aguirre Traders', 'Quasi quae non vel e'),
+(65, 2024702, 'Brown and Goodwin Associates', 'Aliquid quae ipsum e');
 
 -- --------------------------------------------------------
 
@@ -471,15 +690,21 @@ INSERT INTO `user_account` (`id`, `account_id`, `precinct_number`, `first_name`,
 --
 
 --
--- Indexes for table `admin_users`
+-- Indexes for table `admin`
 --
-ALTER TABLE `admin_users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_username`);
 
 --
 -- Indexes for table `announcement`
 --
 ALTER TABLE `announcement`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `change_logs`
+--
+ALTER TABLE `change_logs`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -507,6 +732,41 @@ ALTER TABLE `inquire`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`member_id`),
+  ADD KEY `campus_id` (`campus_id`);
+
+--
+-- Indexes for table `member_account`
+--
+ALTER TABLE `member_account`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `members_id` (`member_id`);
+
+--
+-- Indexes for table `member_address`
+--
+ALTER TABLE `member_address`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `members_id` (`member_id`);
+
+--
+-- Indexes for table `member_emergency`
+--
+ALTER TABLE `member_emergency`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `members_id` (`member_id`);
+
+--
+-- Indexes for table `member_proof`
+--
+ALTER TABLE `member_proof`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `members_id` (`member_id`);
+
+--
 -- Indexes for table `officials`
 --
 ALTER TABLE `officials`
@@ -517,6 +777,13 @@ ALTER TABLE `officials`
 --
 ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `proof`
+--
+ALTER TABLE `proof`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `members_id` (`member_id`);
 
 --
 -- Indexes for table `request_assistant`
@@ -575,16 +842,16 @@ ALTER TABLE `user_account`
 --
 
 --
--- AUTO_INCREMENT for table `admin_users`
---
-ALTER TABLE `admin_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `announcement`
 --
 ALTER TABLE `announcement`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `change_logs`
+--
+ALTER TABLE `change_logs`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `client_login`
@@ -608,7 +875,31 @@ ALTER TABLE `history`
 -- AUTO_INCREMENT for table `inquire`
 --
 ALTER TABLE `inquire`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `member_account`
+--
+ALTER TABLE `member_account`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `member_address`
+--
+ALTER TABLE `member_address`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `member_emergency`
+--
+ALTER TABLE `member_emergency`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `member_proof`
+--
+ALTER TABLE `member_proof`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `officials`
@@ -621,6 +912,12 @@ ALTER TABLE `officials`
 --
 ALTER TABLE `projects`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `proof`
+--
+ALTER TABLE `proof`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `request_assistant`
@@ -667,6 +964,12 @@ ALTER TABLE `settings`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `member_proof`
+--
+ALTER TABLE `member_proof`
+  ADD CONSTRAINT `member_proof_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `request_brgyclrs`
