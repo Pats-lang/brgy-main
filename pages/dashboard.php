f5dceb6be43403637257667a70603b955f47a6b1
Pats-lang <patriciapascual031@gmail.com>
2024-04-18 00:33:45 +0800

    commit

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -10,7 +10,7 @@ include '../server/admin_login-verification.php';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Barangay Management System</title>
+  <title>EGBMS | E-Governance Barangay Management System</title>
   
 
   <?php include 'import.php'; ?>
