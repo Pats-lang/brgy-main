7f5dcb4501efdbfbbc9ee56b2fd662ce5728377f
Pats-lang <128126754+Pats-lang@users.noreply.github.com>
2024-04-25 06:40:49 +0800

    Update viewpdf_brgycoi.php

--- server/viewpdf_brgycoi.php
+++ server/viewpdf_brgycoi.php
@@ -3,7 +3,31 @@
 
 require_once('../plugins/TCPDF-main/tcpdf.php');
 require_once '../plugins/phpqrcode/qrlib.php'; // Include the necessary files for QR code generation
+
 // Ensure no output sent before generating PDF
+
+// Generate unique IDs for each dynamic data
+$uniqueID1 = uniqid();
+$uniqueID2 = uniqid();
+
+// Dynamic data to be included in the QR code
+$dynamicData1 = 'Dynamic Data 1: ' . $uniqueID1;
+$dynamicData2 = 'GENERATED FILE: BARANGGAY CLEARANCE ' ;
+
+// Combine both dynamic data fields
+$combinedDynamicData = $dynamicData1 . "\n" . $dynamicData2;
+
+// Path to save the generated QR code image
+$filename = 'qrcode_' . $uniqueID1 . '_' . $uniqueID2 . '.png';
+
+// QR code size (1 to 10, default is 3)
+$size = 5;
+
+// Error correction level (L, M, Q, H)
+$errorCorrectionLevel = 'L';
+
+// Generate QR code and save the image
+QRcode::png($combinedDynamicData, $filename, $errorCorrectionLevel, $size);
 
 // Create a TCPDF object
 $pdf = new TCPDF('P', 'px', 'A4', true, 'UTF-8');
@@ -31,33 +55,6 @@ $pdf->Image($imagePath1, $x1 = 70, $y1 = 60, $w1 = 75, $h1 = 75, '', '', '', fal
 
 $imagePath2 = 'https://i.ibb.co/dfT9dz7/barangay.jpg'; // Replace with the actual path to your first image file
 $pdf->Image($imagePath2, $x1 = 450, $y1 = 55, $w1 = 85, $h1 = 85, '', '', '', false, 300, '', false, false, false, false, false, false);
-
-
-// Generate unique IDs for each dynamic data
-$uniqueID1 = uniqid();
-$uniqueID2 = uniqid();
-
-
-
-
-// Dynamic data to be included in the QR code
-$dynamicData1 = 'Dynamic Data 1: ' . $uniqueID1;
-$dynamicData2 = 'GENERATED FILE: BARANGGAY CLEARANCE ' ;
-
-// Combine both dynamic data fields
-$combinedDynamicData = $dynamicData1 . "\n" . $dynamicData2;
-
-// Path to save the generated QR code image
-$filename = 'qrcode_' . $uniqueID1 . '_' . $uniqueID2 . '.png';
-
-// QR code size (1 to 10, default is 3)
-$size = 5;
-
-// Error correction level (L, M, Q, H)
-$errorCorrectionLevel = 'L';
-
-// Generate QR code and save the image
-QRcode::png($combinedDynamicData, $filename, $errorCorrectionLevel, $size);
 
 // Set position for the QR code image on the right side, aligned with the Secretary
 $imagePath3 = $filename;
